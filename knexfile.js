2b4c016027c3133380473b60f7b49fef9b8dad8b
yosaku04 <rick.wan@nirvana-info.com>
2025-05-16 16:52:10 +0800

    Add initApp usage and modify connection in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,27 +1,22 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/databases.html
 
 const {
-  DATABASE_URL,
-  ORCA_PG_USER = 'warrior',
-  ORCA_PG_PASSWORD = 'W-x3biMc9cZF',
-  ORCA_PG_HOST = 'localhost',
-  ORCA_PG_PORT = '5432',
-  ORCA_PG_DATABASE = 'orca_sim'
+  ORCA_PG_USER,
+  ORCA_PG_PASSWORD,
+  ORCA_PG_HOST,
+  ORCA_PG_PORT,
+  ORCA_PG_DATABASE
 } = process.env
+
+const connection = `postgres://${ORCA_PG_USER}:${ORCA_PG_PASSWORD}@${ORCA_PG_HOST}:${ORCA_PG_PORT}/${ORCA_PG_DATABASE}`
+
+// Log connection info (password masked)
+const masked = connection.replace(/:\S+@/, ':****@')
+console.log('[knexfile] Using database:', masked)
 
 export default {
   client: 'pg',
-  connection:
-    DATABASE_URL ||
-    `postgres://${ORCA_PG_USER}:${ORCA_PG_PASSWORD}@${ORCA_PG_HOST}:${ORCA_PG_PORT}/${ORCA_PG_DATABASE}`,
-  // Log connection info (password masked) when knexfile is loaded
-  // eslint-disable-next-line no-console
-  ...(function () {
-    const full = DATABASE_URL || `postgres://${ORCA_PG_USER}:${ORCA_PG_PASSWORD}@${ORCA_PG_HOST}:${ORCA_PG_PORT}/${ORCA_PG_DATABASE}`
-    const masked = full.replace(/:\S+@/, ':****@')
-    console.log('[knexfile] Using database:', masked)
-    return {}
-  })(),
+  connection,
   migrations: {
     directory: './migrations',
     tableName: 'knex_migrations'
