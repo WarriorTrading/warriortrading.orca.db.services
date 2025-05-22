baff070d87c60ada77cceee84bc690973eab1cf9
Lucian <lucian.luo@nirvana-info.com>
2025-05-22 10:36:47 +0800

    Modify the configuration and add adaptation environment variables to determine whether to enable SSL

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,8 @@ const {
   ORCA_PG_PASSWORD,
   ORCA_PG_HOST,
   ORCA_PG_PORT,
-  ORCA_PG_DATABASE
+  ORCA_PG_DATABASE,
+  ORCA_PG_SSL
 } = process.env
 
 const connection = `postgres://${ORCA_PG_USER}:${ORCA_PG_PASSWORD}@${ORCA_PG_HOST}:${ORCA_PG_PORT}/${ORCA_PG_DATABASE}`
@@ -14,14 +15,23 @@ const connection = `postgres://${ORCA_PG_USER}:${ORCA_PG_PASSWORD}@${ORCA_PG_HOS
 const masked = connection.replace(/:\S+@/, ':****@')
 console.log('[knexfile] Using database:', masked)
 
+const connectionConfig = {
+  connectionString: connection,
+}
+
+// Only add SSL configuration if ORCA_PG_SSL is true
+if (ORCA_PG_SSL === 'true') {
+  connectionConfig.ssl = {
+    rejectUnauthorized: false,
+  }
+  console.log('[knexfile] SSL is enabled')
+} else {
+  console.log('[knexfile] SSL is disabled')
+}
+
 export default {
   client: 'pg',
-  connection: {
-    connectionString: connection,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  connection: connectionConfig,
   migrations: {
     directory: './migrations',
     tableName: 'knex_migrations'
